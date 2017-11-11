c98451210a041fb4e4c8311e60b1a7def71eac8a
ehterwie <ehterwie@uwaterloo.ca>
2017-11-11 02:29:17 -0500

    HomeActivity layout done

---
+++ PhoneProtectionApp/app/src/main/java/erikterwiel/phoneprotection/AddUserActivity.java
@@ -0,0 +1,13 @@
+package erikterwiel.phoneprotection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+    }
+}
--- PhoneProtectionApp/app/src/main/java/erikterwiel/phoneprotection/HomeActivity.java
+++ PhoneProtectionApp/app/src/main/java/erikterwiel/phoneprotection/HomeActivity.java
@@ -1,8 +1,14 @@
 package erikterwiel.phoneprotection;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
@@ -13,6 +19,9 @@ public class HomeActivity extends AppCompatActivity {
     private static final String TAG = "HomeActivity.java";
     private static final String POOL_ID_UNAUTH = "us-east-1:d2040261-6a0f-4cba-af96-8ead1b66ec38";
 
+    private MenuItem mSettings;
+    private FloatingActionButton mAdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -20,13 +29,26 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        TextView test = (TextView) findViewById(R.id.test);
-        test.setText(getIntent().getStringExtra("username"));
-
         CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
                 getApplicationContext(),
                 POOL_ID_UNAUTH,
                 Regions.US_EAST_1);
 
+        mAdd = (FloatingActionButton) findViewById(R.id.home_add);
+        mAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addUserIntent = new Intent(HomeActivity.this, AddUserActivity.class);
+                startActivity(addUserIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.home_settings, menu);
+        mSettings = menu.findItem(R.id.home_settings);
+        return super.onCreateOptionsMenu(menu);
     }
 }
