10ad973b1f184d5f39ad6282571613322471f1a0
ehterwie <ehterwie@uwaterloo.ca>
2018-02-05 13:54:41 -0500

    Added regular toolbar

--- PhoneProtectionApp/app/src/main/java/erikterwiel/phoneprotection/SettingsActivity.java
+++ PhoneProtectionApp/app/src/main/java/erikterwiel/phoneprotection/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ActionMenuView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,23 +14,18 @@ public class SettingsActivity extends AppCompatActivity {
 
     private static final String TAG = "SettingsActivity.java";
 
-    private ActionMenuView mMenu;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate() called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.settingsToolbar);
-        mMenu = (ActionMenuView) toolbar.findViewById(R.id.toolbar_menu);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(null);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.settings_back, mMenu.getMenu());
+        menuInflater.inflate(R.menu.settings_done, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
