d18085d54f8cee73c1a8a4d801c52dfef2953f22
ehterwie <ehterwie@uwaterloo.ca>
2018-05-01 05:39:44 -0400

    Added Rekognition singleton

---
+++ PhoneProtectionApp/app/src/main/java/erikterwiel/phoneprotection/Rekognition.java
@@ -0,0 +1,40 @@
+package erikterwiel.phoneprotection;
+
+import android.content.Context;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.rekognition.AmazonRekognitionClient;
+
+import static erikterwiel.phoneprotection.DynamoDBKeys.POOL_ID_UNAUTH;
+import static erikterwiel.phoneprotection.DynamoDBKeys.POOL_REGION;
+
+class Rekognition {
+
+    private static Rekognition instance;
+    private AmazonRekognitionClient rekognitionClient;
+
+    private Rekognition(Context context) {
+        AWSCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                context,
+                POOL_ID_UNAUTH,
+                Regions.fromName(POOL_REGION));
+        rekognitionClient = new AmazonRekognitionClient(credentialsProvider);
+    }
+
+    public static void init(Context context) {
+        if (instance == null) {
+            context = context.getApplicationContext();
+            instance = new Rekognition(context);
+        }
+    }
+
+    public static Rekognition getInstance() {
+        return instance;
+    }
+
+    public AmazonRekognitionClient getRekognitionClient() {
+        return rekognitionClient;
+    }
+}
